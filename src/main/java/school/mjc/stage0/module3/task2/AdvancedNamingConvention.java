d84e804ad1627dfc1102cd81cd9a46a496f1fce3
Amachuarori <52317097+Amachuarori@users.noreply.github.com>
2023-10-30 14:28:57 +0600

    my

--- src/main/java/school/mjc/stage0/module3/task2/AdvancedNamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/AdvancedNamingConvention.java
@@ -1,16 +1,16 @@
 package school.mjc.stage0.module3.task2;
 
-public class advancedNamingConvention {
-    private static final int adult_age = 18;
+public class AdvancedNamingConvention {
+    private static final int adult_Age = 18;
 
-    private int AGE;
-    private int phoneNumber;
+    private int _age;
+    private int _phoneNumber;
 
-    void CAllToFriend() {
+    void CallToFriend() {
         //some code
     }
 
-    void callByNumber(int Number) {
+    void CallByNumber(int number) {
         //some code
     }
 }
